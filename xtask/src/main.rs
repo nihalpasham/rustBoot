39d7b25457cf216e207e0607dcf30a02dae2e11d
nihalpasham <nihal.pasham@gmail.com>
2022-05-26 15:09:13 +0530

    ci and xtask fixes

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -51,7 +51,11 @@ fn build_rustBoot_only(target: &&str) -> Result<(), anyhow::Error> {
                                                   // if Path::new("kernel8.img").exists() {
                                                   //     cmd!("powershell -command \"del kernel8.img\"").run()?;
                                                   // }
+            #[cfg(feature = "windows")]
             cmd!("rust-objcopy --strip-all -O binary ..\\..\\target\\aarch64-unknown-none-softfloat\\release\\kernel rustBoot.bin").run()?;
+            #[cfg(not(feature = "windows"))]
+            cmd!("rust-objcopy --strip-all -O binary ../../target/aarch64-unknown-none-softfloat/release/kernel rustBoot.bin").run()?;
+
         }
         &"nrf52840" => {
             cmd!("cargo build --release").run()?;
