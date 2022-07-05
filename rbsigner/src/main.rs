d4a4e21f190bb7a8d670433ce6ddca2d7b2de816
Strange21 <gedamanand19@gmail.com>
2022-07-05 17:26:27 +0530

    added test case for the signing image type length

--- rbsigner/src/main.rs
+++ rbsigner/src/main.rs
@@ -1,29 +1,46 @@
+mod curve;
 mod fitsigner;
+mod mcusigner;
 
-use fitsigner::{import_signing_key, CurveType};
+use curve::SigningKeyType;
+use curve::{import_signing_key, CurveType};
+use fitsigner::sign_fit;
+use mcusigner::sign_mcu_image;
+
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::{Read, Write};
 
-use crate::fitsigner::sign_fit;
-
 fn main() {
     let args = env::args().collect::<Vec<_>>();
     let args = args.iter().map(|s| &**s).collect::<Vec<_>>();
 
-    let mut buf1 = Vec::new();
-    let mut buf2 = Vec::new();
-    let mut itb = fs::File::open(args[1]).expect("Need path to itb_blob as argument");
+    let mut key_file = Vec::new();
     let mut kf = fs::File::open(args[2]).expect("Need path to key_file as argument");
-    itb.read_to_end(&mut buf1).unwrap();
-    kf.read_to_end(&mut buf2).unwrap();
+    kf.read_to_end(&mut key_file).unwrap();
+    let sk: SigningKeyType;
 
-    let kf = buf2.as_slice();
-    let _signed_fit = match args[3] {
+    match args[3] {
         "nistp256" => {
-            let sk = import_signing_key(CurveType::NistP256, &kf[0x40..]).unwrap();
-            let signed_fit = sign_fit(CurveType::NistP256, buf1, sk);
+            let signing_key = &key_file.as_slice()[0x40..];
+            if signing_key.len() != 32 {
+                panic!("invalid nistp256 key: length is not 32 bytes")
+            }
+            sk = import_signing_key(CurveType::NistP256, signing_key).unwrap();
+        }
+        _ => {
+            unimplemented!()
+        }
+    }
+
+    let mut image_blob = Vec::new();
+    match args[0] {
+        "fit-image" => {
+            let mut itb = fs::File::open(args[1]).expect("Need path to itb_blob as argument");
+            itb.read_to_end(&mut image_blob).unwrap();
+
+            let signed_fit = sign_fit(image_blob, sk);
             match signed_fit {
                 Ok(val) => {
                     // println!(
@@ -45,6 +62,29 @@ fn main() {
                 Err(_e) => {}
             }
         }
+        "mcu-image" => {
+            let mut mcu_image =
+                fs::File::open(args[1]).expect("Need path to mcu_image binary as argument");
+            mcu_image.read_to_end(&mut image_blob).unwrap();
+
+            let mcu_image = sign_mcu_image(image_blob, args[1], sk);
+            match mcu_image {
+                Ok(val) => {
+                    let file =
+                        File::create("../boards/rbSigner/signed_images/signed-xyz-name");
+                    match file {
+                        Ok(mut file) => {
+                            let bytes_written = file.write(val.as_slice());
+                            if let Ok(val) = bytes_written {
+                                println!("bytes_written: {:?}", val);
+                            }
+                        }
+                        Err(e) => panic!("error: {:?}", e),
+                    }
+                }
+                Err(_e) => {}
+            }
+        }
         _ => {}
-    };
+    }
 }
