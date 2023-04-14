41ae7bb0d73e50c2a9b136ead0c6bd988f20a2ca
nihalpasham <nihal.pasham@gmail.com>
2023-04-14 11:35:14 +0530

    hal(imx8mn/aarch64-cpu): fixed submodule conflicts

--- boards/hal/src/nxp/imx8mn/aarch64-cpu/target/debug/deps/tock_registers-5a8c132c84b6cfe9.d
+++
@@ -1,9 +0,0 @@
-/Users/nihal.pasham/devspace/rust/projects/exp/uboot-imx/u-boot/aarch64-cpu/target/debug/deps/tock_registers-5a8c132c84b6cfe9.rmeta: /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/lib.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/fields.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/interfaces.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/macros.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/local_register.rs
-
-/Users/nihal.pasham/devspace/rust/projects/exp/uboot-imx/u-boot/aarch64-cpu/target/debug/deps/tock_registers-5a8c132c84b6cfe9.d: /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/lib.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/fields.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/interfaces.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/macros.rs /Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/local_register.rs
-
-/Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/lib.rs:
-/Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/fields.rs:
-/Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/interfaces.rs:
-/Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/macros.rs:
-/Users/nihal.pasham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tock-registers-0.8.1/src/local_register.rs:
